29154fe41f7a59b116a87de92e51d5b65743b870
sagi david <sagi19david@gmail.com>
2024-07-02 13:42:48 +0300

    Build the issue status badge

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,39 +1,47 @@
-import React from 'react'
-import { Button, Table } from '@radix-ui/themes'
-import Link from 'next/link';
-import prisma from '@/prisma/client';
+import React from "react";
+import { Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import prisma from "@/prisma/client";
+import IssueStatusBadge from "../components/IssueStatusBadge";
 
 async function IssuePage() {
   const issues = await prisma.issue.findMany();
   return (
     <div>
-      <div className='mb-5'>
+      <div className="mb-5">
         <Button>
-          <Link href={'/issues/new'}>New Issue</Link>
+          <Link href={"/issues/new"}>New Issue</Link>
         </Button>
       </div>
-      <Table.Root variant='surface'>
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <Table.Cell>Issue</Table.Cell>
-            <Table.Cell className='hidden md:table-cell'>Status</Table.Cell>
-            <Table.Cell className='hidden md:table-cell'>Created</Table.Cell>
+            <Table.Cell className="hidden md:table-cell">Status</Table.Cell>
+            <Table.Cell className="hidden md:table-cell">Created</Table.Cell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {issues.map(issue => (
+          {issues.map((issue) => (
             <Table.Row key={issue.id}>
-              <Table.Cell>{issue.title}
-                <div className='block md:hidden'>{issue.ststus}</div>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.ststus} />
+                </div>
               </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.ststus}</Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.ststus} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
       </Table.Root>
     </div>
-  )
+  );
 }
 
 export default IssuePage;
