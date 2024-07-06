62c1352312ee07b6ef0d7d5fb9ebbd800bf8ed04
sagi david <sagi19david@gmail.com>
2024-07-06 18:37:17 +0300

    Show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoadingIssueDetailPage() {
+  return (
+    <div>Loading...</div>
+  )
+}
+
+export default LoadingIssueDetailPage
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoadingNewIssuePage() {
+  return (
+    <div>Loading...</div>
+  )
+}
+
+export default LoadingNewIssuePage
