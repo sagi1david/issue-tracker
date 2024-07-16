2921d1e31bcf99793765cb2caceb0daeb4597e3c
sagi david <sagi19david@gmail.com>
2024-07-16 15:49:44 +0300

    Secure the application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,16 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
+import authOptions from "@/app/auth/authOptions";
 import prisma from "@/prisma/client";
-import { issueSchema } from '../../validationSchemas';
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from "../../validationSchemas";
 
 export async function POST(request: NextRequest) {
-    const body = await request.json();
-    const validation = issueSchema.safeParse(body);
-    if (!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400 })
+  const session = await getServerSession(authOptions);
 
-    const newIssue = await prisma.issue.create({
-        data: { title: body.title, description: body.description }
-    });
+  if (!session) return NextResponse.json({}, { status: 401 });
 
-    return NextResponse.json(newIssue, { status: 201 });
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
 }
