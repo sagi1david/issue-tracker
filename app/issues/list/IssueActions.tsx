75e15cf2284f96f8996101dfa6597a587604b5e7
sagi david <sagi19david@gmail.com>
2024-07-25 19:08:46 +0300

    Refactor: Extract IssueTable

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
-import React from "react";
 import IssueStatusFilter from "./IssueStatusFilter";
 
 function IssueActions() {
   return (
-    <Flex mb="5" justify="between">
+    <Flex justify="between">
       <IssueStatusFilter />
       <Button>
         <Link href={"/issues/new"}>New Issue</Link>
